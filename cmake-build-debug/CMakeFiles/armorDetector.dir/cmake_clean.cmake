cb2b96585bd5ac35d36573e95d2bd71ef55359ae
YeAnbang <44796419+YeAnbang@users.noreply.github.com>
2019-06-25 20:01:05 +0800

    armorDetector
    see https://github.com/ObitoLee/robomasters_base/blob/maste for original code

---
+++ cmake-build-debug/CMakeFiles/armorDetector.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/armorDetector.dir/main.cpp.obj"
+  "armorDetector.pdb"
+  "armorDetector.exe"
+  "armorDetector.exe.manifest"
+  "libarmorDetector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armorDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
