5b2af5b6cec7ac726654a58afcf222473ac692aa
ZhuKerui <kerui.17@intl.zju.edu.cn>
2019-07-05 21:18:02 +0800

    blablabla

---
+++ src/interfaces/vision_port.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kerui Zhu on 7/4/2019.
+//
+
+#include "vision_port.h"
---
+++ src/interfaces/vision_port.h
@@ -0,0 +1,14 @@
+//
+// Created by Kerui Zhu on 7/4/2019.
+//
+
+#ifndef META_VISION_VISION_PORT_H
+#define META_VISION_VISION_PORT_H
+
+
+class VisionPort {
+
+};
+
+
+#endif //META_VISION_VISION_PORT_H
