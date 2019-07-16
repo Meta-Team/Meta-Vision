c706ce03634de87df41dfeb8dffe66b5d9d3b459
liuzikai <liuzikai@163.com>
2019-07-16 16:43:54 +0800

    Revise settings.

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ int Main::main(int argc, char **argv) {
     }
 
     _serial = new SerialStatus(
-            config["system"]["serial"]["device"].as<string>(), B115200
+            config["system"]["serial"]["device"].as<string>(), config["system"]["serial"]["baudrate"].as<int>()
     );
 
     Mat frame, resized;
