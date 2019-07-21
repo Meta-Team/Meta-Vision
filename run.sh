05ceaca6c8b7caaf37ca8c5f1669c5ee3bdb3264
liuzikai <liuzikai@163.com>
2019-07-21 17:28:13 +0800

    Small revision.

--- run.sh
+++ run.sh
@@ -5,5 +5,22 @@ cd /home/meta/Desktop/Meta-Vision/build
 log_filename=`date +%Y-%m-%d_%H-%M-%S`
 
 while
-    cmake .. && make Vision -j4 && ./Vision ../test.yml | tee "../log/${log_filename}.txt"
+
+    echo "---------------------- CMAKE -------------------------"
+    cmake ..
+
+    echo
+    echo "----------------------- MAKE -------------------------"
+    make Vision -j4
+
+    echo
+    echo "------------------------ RUN -------------------------"
+    ./Vision ../jetson.yml | tee "../log/${log_filename}.txt"
+
+    echo
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "                        RESTART                        "
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo
+
 do :; done
