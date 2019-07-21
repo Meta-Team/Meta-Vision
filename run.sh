ccdfbf1eaab7c3ad4f3f4b7e59e0e3d5ba93ff4d
liuzikai <liuzikai@163.com>
2019-07-21 16:36:52 +0800

    Revise shell script.

--- run.sh
+++ run.sh
@@ -5,4 +5,8 @@ cd /home/meta/Desktop/Meta-Vision/build
 mkdir log
 
 log_file=`date +%Y-%m-%d_%H-%M-%S.log`
-cmake .. && make Vision -j4 && sudo ./Vision ../jetson.yml | tee log_file
+
+cmake ..
+make Vision -j4
+
+sudo ./Vision ../jetson.yml | tee "log/${log_file}"
