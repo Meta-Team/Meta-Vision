45d6cad122873ff6fc63ffd862c69ae03ecf36ca
liuzikai <liuzikai@163.com>
2019-07-21 16:56:13 +0800

    Add shell script.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /home/meta/Desktop/Meta-Vision/build
+
+log_filename=`date +%Y-%m-%d_%H-%M-%S`
+
+while
+    cmake .. && make Vision -j4 && sudo ./Vision ../jetson_test.yml | tee "../log/${log_filename}.txt"
+do :; done
